bea78dad35bfac43eb08ad3cbc0ba072af5a821b
Pallavi0595 <pallavmishra.ranchi@gmail.com>
2025-11-19 12:16:05 +0530

    meet expert teams fixed

--- src/components/homepage/MeetOurExpertTeam/ExpertTeam.jsx
+++ src/components/homepage/MeetOurExpertTeam/ExpertTeam.jsx
@@ -28,33 +28,32 @@ import TeamRightBoxes from "./TeamRightBoxes";
 export default function ExpertTeam() {
   return (
     <div className="w-full flex justify-center items-center">
-       
-       <div
-      style={{
-        background:
-          "linear-gradient(45deg, #BD8668 0%, #5D51AF 50%, #3437C8 100%)",
-          
-        borderRadius: "24px",
-        padding: "74px 48px",
-        marginTop: "40px",
-        width: " 1260px",
-      }}
-    >
       <div
         style={{
-          display: "flex",
-          width: "1260px",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "flex-start",
-          gap: "10px",
-          margin: "0 auto",
+          background:
+            "linear-gradient(45deg, #BD8668 0%, #5D51AF 50%, #3437C8 100%)",
+
+          borderRadius: "24px",
+          padding: "74px 48px",
+          marginTop: "40px",
+          // width: " 1260px",
         }}
       >
-        <TeamLeftContent />
-        <TeamRightBoxes />
+        <div
+          style={{
+            display: "flex",
+            width: "1260px",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "flex-start",
+            gap: "10px",
+            margin: "0 auto",
+          }}
+        >
+          <TeamLeftContent />
+          <TeamRightBoxes />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
